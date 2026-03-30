886ceaffeb6c07f342a5dbd4a6f5342174f2961e
kkuoii007-coder <kkuoii007@gmail.com>
2026-03-30 16:21:57 +0300

    7

--- app/models.py
+++ app/models.py
@@ -1,1 +1,17 @@
-from app
+#создаем базу данных
+from app import db
+from app import login_manager
+from flask_login import UserMixin
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_rey=True)
+    username = db.Column(db.String(100), unigue=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    clicks = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return f'User {self.username} - clicks: {self.clicks}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
